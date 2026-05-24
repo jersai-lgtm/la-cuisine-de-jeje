458b3bc8b89dff63489d9fac40149bd857102462
jersai-lgtm <jerome.sainthot@gmail.com>
2026-05-24 09:49:17 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,67 @@
-const CACHE_NAME = "cuisine-jeje-v21";
+const CACHE_NAME = "cuisine-jeje-v22";
 const FICHIERS = [
   "/la-cuisine-de-jeje/",
   "/la-cuisine-de-jeje/index.html",
   "/la-cuisine-de-jeje/style.css",
   "/la-cuisine-de-jeje/script.js",
   "/la-cuisine-de-jeje/manifest.json",
-  "/la-cuisine-de-jeje/images/fond.jpg",
-  "/la-cuisine-de-jeje/images/pizza.jpg",
-  "/la-cuisine-de-jeje/images/crepes.jpg",
-  "/la-cuisine-de-jeje/images/gaufres.jpg",
-  "/la-cuisine-de-jeje/images/brioche.jpg",
-  "/la-cuisine-de-jeje/images/lasagne.jpg",
-  "/la-cuisine-de-jeje/images/cookies.jpg",
-  "/la-cuisine-de-jeje/images/flan.jpg",
-  "/la-cuisine-de-jeje/images/clafoutis.jpg",
   "/la-cuisine-de-jeje/images/icon-192.png",
-  "/la-cuisine-de-jeje/images/icon-512.png"
+  "/la-cuisine-de-jeje/images/icon-512.png",
+  "/la-cuisine-de-jeje/images/fond.webp",
+  "/la-cuisine-de-jeje/images/pizza.webp",
+  "/la-cuisine-de-jeje/images/crepes.webp",
+  "/la-cuisine-de-jeje/images/gaufres.webp",
+  "/la-cuisine-de-jeje/images/brioche.webp",
+  "/la-cuisine-de-jeje/images/lasagne.webp",
+  "/la-cuisine-de-jeje/images/cookies.webp",
+  "/la-cuisine-de-jeje/images/flan.webp",
+  "/la-cuisine-de-jeje/images/clafoutis.webp",
+  "/la-cuisine-de-jeje/images/tiramisu.webp",
+  "/la-cuisine-de-jeje/images/tarteaupommes.webp",
+  "/la-cuisine-de-jeje/images/tartecitron.webp",
+  "/la-cuisine-de-jeje/images/goumeau.webp",
+  "/la-cuisine-de-jeje/images/pancakes.webp",
+  "/la-cuisine-de-jeje/images/muffins.webp",
+  "/la-cuisine-de-jeje/images/croquemonsieur.webp",
+  "/la-cuisine-de-jeje/images/madeleine.webp",
+  "/la-cuisine-de-jeje/images/bananabread.webp",
+  "/la-cuisine-de-jeje/images/granola.webp",
+  "/la-cuisine-de-jeje/images/smoothiebowl.webp",
+  "/la-cuisine-de-jeje/images/saladequinoa.webp",
+  "/la-cuisine-de-jeje/images/yaourt.webp",
+  "/la-cuisine-de-jeje/images/veloutelegumes.webp",
+  "/la-cuisine-de-jeje/images/houmous.webp",
+  "/la-cuisine-de-jeje/images/boeufbourguignon.webp",
+  "/la-cuisine-de-jeje/images/risotto.webp",
+  "/la-cuisine-de-jeje/images/gratindauphinois.webp",
+  "/la-cuisine-de-jeje/images/cremebrulee.webp",
+  "/la-cuisine-de-jeje/images/mousseauchocolat.webp",
+  "/la-cuisine-de-jeje/images/ileflottante.webp",
+  "/la-cuisine-de-jeje/images/fondantchocolat.webp",
+  "/la-cuisine-de-jeje/images/painbaguette.webp",
+  "/la-cuisine-de-jeje/images/paindemie.webp",
+  "/la-cuisine-de-jeje/images/patefeuilletee.webp",
+  "/la-cuisine-de-jeje/images/patebrisee.webp",
+  "/la-cuisine-de-jeje/images/patesablee.webp",
+  "/la-cuisine-de-jeje/images/saladeniçoise.webp",
+  "/la-cuisine-de-jeje/images/saladecesar.webp",
+  "/la-cuisine-de-jeje/images/saladegreque.webp",
+  "/la-cuisine-de-jeje/images/saladepatasthon.webp",
+  "/la-cuisine-de-jeje/images/saladerizmediterranee.webp",
+  "/la-cuisine-de-jeje/images/tabulemaison.webp",
+  "/la-cuisine-de-jeje/images/saladelentilles.webp",
+  "/la-cuisine-de-jeje/images/saladeavocatcrevettes.webp",
 ];
 
 // Installation : mise en cache de tous les fichiers
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FICHIERS))
+    caches.open(CACHE_NAME).then(cache => {
+      // On cache les fichiers un par un pour ne pas bloquer si une image manque
+      return Promise.allSettled(
+        FICHIERS.map(f => cache.add(f).catch(() => console.log("Cache skip:", f)))
+      );
+    })
   );
   self.skipWaiting();
 });
