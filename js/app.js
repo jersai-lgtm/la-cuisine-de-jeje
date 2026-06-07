321628d16d7ab22843186693074561c6fd36f083
jersai-lgtm <jerome.sainthot@gmail.com>
2026-06-07 15:00:54 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -871,7 +871,7 @@ function miniCarteFetiche(key, count) {
     ${badgeFam}
     ${badgeFreq}
     <div class="mini-carte-info">
-      <span class="mini-carte-emoji">${r.emoji || "🍽️"}</span>
+      <span class="mini-carte-entete"><span class="mini-carte-emoji">${r.emoji || "🍽️"}</span>${typeof drapeau === "function" ? drapeau(r.pays, 13) : ""}</span>
       <span class="mini-carte-nom">${nom}</span>
       <span class="mini-carte-temps">⏱ ${r.temps || ""}</span>
     </div>
